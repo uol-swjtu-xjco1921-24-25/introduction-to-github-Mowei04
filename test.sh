3fde02e90d50e9e5174ab9799ab94c19cfdc309d
Mowei04 <2770237136@qq.com>
2025-04-02 11:43:23 +0800

    the test for no arguments

--- test.sh
+++ test.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-echo -e "/n/n~~ Argument Test ~~"
+echo -e "\n\n~~ Argument Test ~~"
 
-echo
+echo -n "Testing no arguments - "
+
+./maze > tmp
+
+if grep -q "Usage: maze <filename>" tmp;
+then 
+    echo "Pass"
+else
+    echo "Fail"
+fi
